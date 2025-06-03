a252943750fb52a1fe59a2fb49635491c83eb8e2
jorangi <shfl3182919@gmail.com>
2025-06-04 08:25:40 +0900

    Swing2nd Animation Adding

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     private Animator anim;
+    public int swingCount = 0;
     public bool isSwing;
     private void Awake()
     {
         anim = GetComponent<Animator>();
     }
     public void StartSwing(){
-        if(anim.GetBool("IsSwing")) return;
+        if (anim.GetBool("IsSwing")) return;
+        anim.SetInteger("SwingCount", (anim.GetInteger("SwingCount") + 1) % 2);
         anim.SetBool("IsSwing", true);
         isSwing = true;
     }
-    public void StopSwing(){
+    public void SwingEnd()
+    {
         anim.SetBool("IsSwing", false);
         isSwing = false;
+    }
+    public void StopSwing()
+    {
+        anim.SetInteger("SwingCount", 0);
     }
 }
