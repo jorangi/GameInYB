05c8a6dd7ccee66b50027f05afcf1e5dc9f830f7
jorangi <shfl3182919@gmail.com>
2025-09-17 13:18:10 +0900

    Test

--- Assets/Scripts/UI/ItemInformationModal.cs
+++ Assets/Scripts/UI/ItemInformationModal.cs
@@ -1,7 +1,22 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ItemInformationModal : MoveableInformationModal
+public class ItemInformationModal : MoveableInformationModal, IPointerClickHandler
 {
+    #region Legacy
+    private TextMeshProUGUI source;
+    private void Awake()
+    {
+        source = transform.Find("description").GetComponent<TextMeshProUGUI>();
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int index = TMP_TextUtilities.FindIntersectingLink(source, eventData.position, eventData.pressEventCamera);
+        Debug.DrawLine(Input.mousePosition, Input.mousePosition + Vector3.left * 10, Color.red);
+        Debug.Log(index);
+    }
+    #endregion
     public void Show(string content, Vector2 screenPos, Camera cam)
     {
         base.Show();
@@ -14,4 +29,5 @@ public class ItemInformationModal : MoveableInformationModal
     {
         
     }
+
 }
